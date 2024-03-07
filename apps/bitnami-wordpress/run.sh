d0624b13145752d8d9af0b17788ccdcdb1626e3b
Mark Alston <malston@vmware.com>
2024-03-07 08:13:22 -0700

    Polish run.sh

--- apps/bitnami-wordpress/run.sh
+++ apps/bitnami-wordpress/run.sh
@@ -6,14 +6,14 @@ set -o pipefail
 __DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 function kustomizeIt {
-  clusterDomain="$2" kubectl kustomize \
+  kubectl kustomize \
     --enable-helm \
     --helm-command helm \
     "$__DIR/$1"
 }
 
 function build {
-  kustomizeIt "$1" "$2"
+  kustomizeIt "$1"
 }
 
 function delete {
@@ -21,22 +21,39 @@ function delete {
 }
 
 function apply {
-  kustomizeIt "$1" "$2" | kubectl apply -f -
+  kustomizeIt "$1" | kubectl apply -f -
+}
+
+function validate_overlay_arg {
+  if [[ -z $1 ]]; then
+    echo "You must specify an overlay to diff: (dev, prod)"
+    exit 1
+  fi
 }
 
 COMMAND=$1
-OVERLAY=overlays/$2
-DOMAIN=$3
+OVERLAY=$2
+if [[ ! $OVERLAY == "base" ]]; then
+  OVERLAY=overlays/$2
+fi
 
 case "$COMMAND" in
-  diff) diff <(kustomizeIt base) <(kustomizeIt "$OVERLAY") | more
-  ;;
-  build) build "$OVERLAY" "$DOMAIN"
-  ;;
-  apply) apply "$OVERLAY" "$DOMAIN"
-  ;;
-  delete) delete "$OVERLAY"
-  ;;
-  *) echo "Please specify a command: (diff, build, apply, delete)"
+  diff)
+    validate_overlay_arg "$2"
+    diff <(kustomizeIt base) <(kustomizeIt "$OVERLAY") | more
+    ;;
+  build)
+    validate_overlay_arg "$2"
+    build "$OVERLAY"
+    ;;
+  apply)
+    validate_overlay_arg "$2"
+    apply "$OVERLAY"
+    ;;
+  delete)
+    validate_overlay_arg "$2"
+    delete "$OVERLAY"
+    ;;
+  *) echo "Please specify a command to run: (diff, build, apply, delete)"
   ;;
 esac
